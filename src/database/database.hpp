707dbe31040b820c9ab28ce4060cc8b168ca1eff
MTRNord <mtrnord1@gmail.com>
2023-11-12 18:20:02 +0100

    Initial events table

--- src/database/database.hpp
+++ src/database/database.hpp
@@ -5,6 +5,8 @@
 
 using namespace soci;
 
+extern "C" void register_factory_postgresql();
+
 /**
  * @brief A wrapper for database ops. This is intentionally stateless and not a
  * cache.
@@ -15,10 +17,16 @@ private:
 
 public:
   Database(std::string db_url, const size_t pool_size) : pool(pool_size) {
+    register_factory_postgresql();
     for (size_t i = 0; i != pool_size; ++i) {
       session &sql = this->pool.at(i);
 
       sql.open(db_url);
     }
   }
+
+  void migrate();
+
+private:
+  void migration_v1();
 };
