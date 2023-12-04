e5ae4bd74f4aa285addb0ea57bc966d02bd6465d
MTRNord <mtrnord1@gmail.com>
2023-12-04 21:27:43 +0100

    Add missing trailing spaces

--- src/database/migrations/migrator.cpp
+++ src/database/migrations/migrator.cpp
@@ -40,11 +40,11 @@ void Migrator::migration_v1() {
     /* Global events table
        This is meant to be accompanied with various views for per-room data */
     auto f1 = transPtr->execSqlAsyncFuture(
-        "CREATE TABLE IF NOT EXISTS events ("
-        "event_id TEXT NOT NULL CONSTRAINT event_id_unique UNIQUE,"
-        "room_id TEXT NOT NULL, depth BIGINT NOT NULL,"
-        "auth_events TEXT[] NOT NULL, rejected BOOLEAN NOT NULL DEFAULT FALSE,"
-        "state_key TEXT, type TEXT NOT NULL, json TEXT NOT NULL"
+        "CREATE TABLE IF NOT EXISTS events ( "
+        "event_id TEXT NOT NULL CONSTRAINT event_id_unique UNIQUE, "
+        "room_id TEXT NOT NULL, depth BIGINT NOT NULL, "
+        "auth_events TEXT[] NOT NULL, rejected BOOLEAN NOT NULL DEFAULT FALSE, "
+        "state_key TEXT, type TEXT NOT NULL, json TEXT NOT NULL "
         ")");
     f1.wait();
 
@@ -58,20 +58,20 @@ void Migrator::migration_v1() {
     /* Create materialized views on insert -- Sadly formating is broken. See
      * migration sql files for readable versions */
     auto f3 = transPtr->execSqlAsyncFuture(
-        "CREATE OR REPLACE FUNCTION new_room_view()"
-        "RETURNS trigger LANGUAGE plpgsql AS $$"
-        "DECLARE room_cleared text;"
-        "BEGIN"
+        "CREATE OR REPLACE FUNCTION new_room_view() "
+        "RETURNS trigger LANGUAGE plpgsql AS $$ "
+        "DECLARE room_cleared text; "
+        "BEGIN "
         "    room_cleared := REPLACE(REPLACE(REPLACE(NEW.room_id, '.', '_'), "
-        "':', '_'), '!', '');"
+        "':', '_'), '!', ''); "
         "    EXECUTE 'CREATE MATERIALIZED VIEW IF NOT EXISTS ' || "
         "quote_ident('room_' || room_cleared) || ' AS SELECT * FROM events "
-        "WHERE room_id = ' || quote_literal(NEW.room_id);"
+        "WHERE room_id = ' || quote_literal(NEW.room_id); "
         "    EXECUTE 'CREATE UNIQUE INDEX IF NOT EXISTS ' || "
         "quote_ident('room_' || room_cleared || '_idx') || ' ON ' || "
-        "quote_ident('room_' || room_cleared) || ' (event_id);';"
-        "    RETURN NULL;"
-        "END;"
+        "quote_ident('room_' || room_cleared) || ' (event_id);'; "
+        "    RETURN NULL; "
+        "END; "
         "$$");
     f3.wait();
 
@@ -84,42 +84,43 @@ void Migrator::migration_v1() {
     /* Create update fn which needs to be called manually as we can batch this
      * in the app logic */
     auto f5 = transPtr->execSqlAsyncFuture(
-        "CREATE OR REPLACE FUNCTION room_view_update(room_id text)"
-        "RETURNS void"
-        "LANGUAGE plpgsql AS $$"
-        "DECLARE room_cleared text;"
-        "BEGIN"
+        "CREATE OR REPLACE FUNCTION room_view_update(room_id text) "
+        "RETURNS void "
+        "LANGUAGE plpgsql AS $$ "
+        "DECLARE room_cleared text; "
+        "BEGIN "
         "    room_cleared := REPLACE(REPLACE(REPLACE(room_id, '.', '_'), ':', "
-        "'_'), '!', '');"
+        "'_'), '!', ''); "
         "    EXECUTE 'REFRESH MATERIALIZED VIEW CONCURRENTLY ' || "
-        "quote_ident('room_' || room_cleared);"
-        "END;"
+        "quote_ident('room_' || room_cleared); "
+        "END; "
         "$$;");
     f5.wait();
 
     /* User View - Create materialized view on insert. This view only contains
      * for each user the membership events */
     auto f6 = transPtr->execSqlAsyncFuture(
-        "CREATE OR REPLACE FUNCTION new_user_view()"
-        "RETURNS trigger LANGUAGE plpgsql AS $$"
-        "DECLARE state_key_cleared text;"
-        "BEGIN"
+        "CREATE OR REPLACE FUNCTION new_user_view() "
+        "RETURNS trigger LANGUAGE plpgsql AS $$ "
+        "DECLARE state_key_cleared text; "
+        "BEGIN "
         "    CASE "
-        "        WHEN NEW.state_key IS NOT NULL THEN"
+        "        WHEN NEW.state_key IS NOT NULL THEN "
         "            state_key_cleared := "
-        "REPLACE(REPLACE(REPLACE(NEW.state_key, '.', '_'), ':', '_'), '@', '');"
+        "REPLACE(REPLACE(REPLACE(NEW.state_key, '.', '_'), ':', '_'), '@', "
+        "''); "
         "            EXECUTE 'CREATE MATERIALIZED VIEW IF NOT EXISTS ' || "
         "quote_ident('user_' || state_key_cleared) || ' AS SELECT * FROM "
         "events WHERE type = ''m.room.member'' AND state_key = ' || "
-        "quote_literal(NEW.state_key);"
+        "quote_literal(NEW.state_key); "
         "            EXECUTE 'CREATE UNIQUE INDEX IF NOT EXISTS ' || "
         "quote_ident('user_' || state_key_cleared || '_idx') || ' ON ' || "
-        "quote_ident('user_' || state_key_cleared) || ' (event_id);';"
-        "        ELSE"
-        "            -- Do nothing"
-        "    END CASE;"
-        "    RETURN NULL;"
-        "END;"
+        "quote_ident('user_' || state_key_cleared) || ' (event_id);'; "
+        "        ELSE "
+        "            /* Do nothing */ "
+        "    END CASE; "
+        "    RETURN NULL; "
+        "END; "
         "$$;");
     f6.wait();
 
@@ -129,16 +130,16 @@ void Migrator::migration_v1() {
     f7.wait();
 
     auto f8 = transPtr->execSqlAsyncFuture(
-        "CREATE OR REPLACE FUNCTION user_view_update(state_key text)"
-        "RETURNS void"
-        "LANGUAGE plpgsql AS $$"
-        "DECLARE state_key_cleared text;"
-        "BEGIN"
+        "CREATE OR REPLACE FUNCTION user_view_update(state_key text) "
+        "RETURNS void "
+        "LANGUAGE plpgsql AS $$ "
+        "DECLARE state_key_cleared text; "
+        "BEGIN "
         "    state_key_cleared := REPLACE(REPLACE(REPLACE(state_key, '.', "
-        "'_'), ':', '_'), '@', '');"
+        "'_'), ':', '_'), '@', ''); "
         "    EXECUTE 'REFRESH MATERIALIZED VIEW CONCURRENTLY ' ||  "
-        "quote_ident('user_' || state_key_cleared);"
-        "END;"
+        "quote_ident('user_' || state_key_cleared); "
+        "END; "
         "$$;");
     f8.wait();
 
@@ -172,16 +173,16 @@ void Migrator::migration_v2() {
     /* Used to generate the users table
      These are LOCAL users only.*/
     auto f1 =
-        transPtr->execSqlAsyncFuture("CREATE TABLE IF NOT EXISTS users ("
-                                     "matrix_id TEXT PRIMARY KEY,"
-                                     "password_hash TEXT NOT NULL,"
-                                     "avatar_url TEXT, display_name TEXT);");
+        transPtr->execSqlAsyncFuture("CREATE TABLE IF NOT EXISTS users ( "
+                                     "matrix_id TEXT PRIMARY KEY, "
+                                     "password_hash TEXT NOT NULL, "
+                                     "avatar_url TEXT, display_name TEXT); ");
     f1.wait();
 
     auto f2 = transPtr->execSqlAsyncFuture(
-        "CREATE TABLE IF NOT EXISTS devices ("
-        "matrix_id TEXT NOT NULL references users(matrix_id),"
-        "device_id TEXT NOT NULL, device_name TEXT NOT NULL,"
+        "CREATE TABLE IF NOT EXISTS devices ( "
+        "matrix_id TEXT NOT NULL references users(matrix_id), "
+        "device_id TEXT NOT NULL, device_name TEXT NOT NULL, "
         "access_token TEXT NOT NULL UNIQUE, PRIMARY KEY(matrix_id, "
         "device_id));");
     f2.wait();
