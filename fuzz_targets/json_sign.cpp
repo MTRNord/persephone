0c2c3371ae8f7755d5fd359b458d092fafb4aba5
MTRNord <mtrnord1@gmail.com>
2024-01-02 12:30:10 +0100

    Ignore c errors in fuzz target

--- fuzz_targets/json_sign.cpp
+++ fuzz_targets/json_sign.cpp
@@ -1,10 +1,12 @@
 #include "utils/json_utils.hpp"
 
+//  This file is essentially C
+
 /* this lets the source compile without afl-clang-fast/lto */
 #ifndef __AFL_FUZZ_TESTCASE_LEN
 
-ssize_t fuzz_len;
-unsigned char fuzz_buf[1024000];
+ssize_t fuzz_len;                                           // skipcq:  CXX-W2009
+unsigned char fuzz_buf[1024000];                            // skipcq: CXX-W2066, CXX-W2009
 // NOLINTBEGIN(bugprone-reserved-identifier,cert-dcl37-c,cert-dcl51-cpp)
 #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
 #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf
@@ -27,6 +29,7 @@ int main() {
   ssize_t len;        /* how much input did we read? */
   unsigned char *buf; /* test case buffer pointer    */
   __AFL_INIT();
+  // skipcq: CXX-C1000
   buf = __AFL_FUZZ_TESTCASE_BUF; // this must be assigned before __AFL_LOOP!
 
   while (__AFL_LOOP(UINT_MAX)) {
